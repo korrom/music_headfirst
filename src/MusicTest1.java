245606f07b3d193df8a32645c413e937ac5ed869
Roman <kororom@bk.ru>
2018-09-05 00:34:01 +0400

    first commit

---
+++ src/MusicTest1.java
@@ -0,0 +1,16 @@
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
+public class MusicTest1 {
+
+    public void play() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("We get sintezator");
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
